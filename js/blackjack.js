649d6bf8181263a6a4c88b9338079e1c188d289a
Justin Nguyen <justin_nguyen@student.uml.edu>
2016-02-11 16:19:24 -0500

    updated blackjack algorithm

--- js/blackjack.js
+++ js/blackjack.js
@@ -1,8 +1,8 @@
 /*                                                                                                                                                         
  Name: Justin Nguyen                                                                                                                                        
- Contact: justinnnguyen@hotmail.com                                                                                                                                                                                                                                                     
+ Contact: justintimenguyen@gmail.com                                                                                                                                                                                                                                                    
  School: University of Massachusetts Lowell                                                                                                                                                                                                                                                                                 
- Date Updated: January 28, 2016                                                                                                                                                                                                                              
+ Date Updated: February 11, 2016                                                                                                                                                                                                                              
  Description: Assignment 9 - Implementing Blackjack using the MVC method        
  Copyright [2016] by Justin Nguyen. All rights reserved.                                                                                                    
  May be freely copied or excerpted for educational purposes with credit to the author.                                                                      
@@ -21,7 +21,8 @@ PlayingCards[8] = { "card": "10", "value": 10, "original_distribution": 4, "numb
 PlayingCards[9] = { "card": "11", "value": 10, "original_distribution": 4, "number_remaining": 4 }; //Card: Jack
 PlayingCards[10] = { "card": "12", "value": 10, "original_distribution": 4, "number_remaining": 4 }; //Card: Queen
 PlayingCards[11] = { "card": "13", "value": 10, "original_distribution": 4, "number_remaining": 4 }; //Card: King
-/*Ace will need to be tested for 1 and 11*/
+//Ace will need to be tested for 1 and 11
+//If score is under 21, Keep it at 11. If the score goes over 21, change it to 1
 PlayingCards[12] = { "card": "14", "value": 1, "original_distribution": 4, "number_remaining": 4 }; //Card: Ace
 
 /*Ready when page is loaded*/
@@ -33,6 +34,7 @@ $(document).ready(function () {
 
 /*Global Variables*/
 var cards = "";
+var dealerCards = ""; //generate dealer hand
 var table = ""; //generate scrabble board
 var score = 0;
 var board = [];
@@ -43,6 +45,7 @@ var display_letters = "";
 
 //TODO: Fix tile placement. The droppable is taking the draggable on the bottom right of it.
 
+//generates cards in the Player's hand
 function generateCards() {
     cards = '';
     var cards_value = '';
@@ -56,69 +59,54 @@ function generateCards() {
         var cardImageUrl = 'img/PlayingCards/PlayingCards';
 
         cards += "<img id='play-cards" + "' class='card-piece" + card_value + 'a' + "' src='" + cardImageUrl + card_value + 'a' + ".png" + "'></img>";
-
-
-        values.push([i, PlayingCards[value].value]);
-        cards_placed.push([i, cards_value]);
-
-        console.log(values[i][1]);
+        
+        //values.push([i, PlayingCards[value].value]);
+        //cards_placed.push([i, cards_value]);
+        //console.log(values[i][1]);
 
         generateScore(PlayingCards[value].value);
-        console.log(cards_value);
+        //console.log(cards_value);
         //$('#score').html(score);
         $('#cards_value').html(cards_value);
         $('#cards').html(cards);
     }
 }
 
+//TODO: fix to use for both the Player and Dealer.
+//      Make it more general
+//generates the score of both the player and dealer
 function generateScore(scoreResult) {
     score += scoreResult;
-    
     //console.log(score);
     $('#score').html(score);
 }
 
-/*TODO: Generate a new card to add to the current 2 cards of the user.*/
+//TODO: Fix how the card are displayed
+//      This should also be taking away values from the deck
+//will generate the cards in the Dealer's hand
+function DealerHand() {
+    dealerCards = '';
+    dealerCardValues = '';
+    for (var i = 0; i < 2; i++) {
+        var value = Math.floor(Math.random() * PlayingCards.length); //randomize 2 cards
+        if (PlayingCards[value].number_remaining === 0) {
+            value = Math.floor(Math.random() * PlayingCards.length);
+        } //end if statement
+        var dealerCardValues = PlayingCards[value].card;
+        dealerCardValues += PlayingCards.card;
+        var cardImageUrl = 'img/PlayingCards/PlayingCards';
+
+        //need to make it able to put both card-values in the array but only display 1 and display a facedown card
+        dealerCards += "<img id='play-cards" + "' class='card-piece" + card_value + 'a' + "' src='" + cardImageUrl + card_value + 'a' + ".png" + "'></img>";
+    } //end forloop
+        dealerCards += "<img id='play-cards class='card-piece' src='" + dealerCardValues + 'a' + ".png" + "></img>";        
+        dealerCards += "<img id='play-cards" + "' class='card-piece" + card_value + 'a' + "' src='" + cardImageUrl + card_value + 'a' + ".png" + "'></img>";
+}
+
+//TODO: Generate a new card to add to the current 2 cards of the user.
+//      Needs to generate the cards out of the pool of remaining cards
 /*
 function blackjackHit() {
     
-}
-*/
-/*function generateCards() {
-    tiles = ""; //inserts the image of the tile corresponding to the letter
-    //alphabet = ""; //keeps track of the randomized letters for testing purposes
-    //alpha_value = ""; //keeps track for value for testing purposes
-
-    //This gets 7 random letters from the ScrabbleTiles array
-    //reference: http://stackoverflow.com/questions/4550505/getting-random-value-from-an-array
-    for (var i = 0; i < 2; i++) {
-        var value = Math.floor(Math.random() * PlayingCards.length); //randomize a letter
-
-        //check if number remaining of random letter is 0, if it is get a new letter  
-        if (PlayingCards[value].number_remaining === 0) {
-            value = Math.floor(Math.random() * PlayingCards.length);
-        }
-        var letter_value = PlayingCards[value].letter;
-        //alphabet += letter_value; //put the letters in an array for testing purposes
-
-        var scrabble_url = "images/Scrabble_Tiles/Scrabble_Tile_";
-        //inserts the id, class and image of the board pieces
-
-        tiles += "<img id='tile-drag_" + i + "' class='card-piece" + letter_value + "' src='" + scrabble_url + letter_value + ".jpg" + "'></img>";
-        //alpha_value += ScrabbleTiles[value].number_remaining -= 1; 
-
-        var numRemaining = PlayingCards[value].number_remaining;
-        numRemaining = numRemaining - 1; //decrement number remaining of that tile
-
-        values.push([i, PlayingCards[value].value]);
-        letters_placed.push([i, letter_value]);
-        //console.log(values[i][1]);
-
-    }
-    $('#score').html(score);
-    $('#cards').html(tiles);
-    //$('#alpha').html(alpha_value);
-    //$('#rack').html(alphabet[3]);
-
 }
 */
